91592a7ab4f9a363b3948b69044eb9920bf589ce
DavAlbar <albardonedo@pm.me>
2024-11-17 21:26:07 +0100

    Delete routes directory

--- routes/api.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-// Rutas para autores
-router.use("/autores", require("./api/autores.routes"));
-
-// Rutas para posts
-router.use("/posts", require("./api/posts.routes"));
-
-module.exports = router;
