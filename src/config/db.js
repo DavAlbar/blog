b9fe599cbd33e08b57e7c69d7d75aad1709d9b7b
DavAlbar <albardonedo@pm.me>
2024-11-17 21:26:57 +0100

    Create db.js

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "rootroot",
+  port: 3306,
+  database: "blog_db",
+});
+
+module.exports = db;
+a;
